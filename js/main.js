da5d56d559c456fd71eb54074138d8a66b5b4ca2
mdabdulkyum1 <kyummdabdul@gmail.com>
2024-09-23 18:16:20 +0600

    mobile menu done

--- js/main.js
+++ js/main.js
@@ -118,3 +118,20 @@ donateBtnQ.addEventListener('click', function(){
 getElById('close-modal').addEventListener('click', function(){
     getElById('modal').classList.add('hidden');
 });
+
+
+
+// toggle nav menu
+const menuOpen = document.getElementById('menu-open');
+const menuClose = document.getElementById('menu-close');
+const dropMenu = document.getElementById('drop-menu');
+menuOpen.addEventListener('click', function(){
+    menuOpen.classList.add('hidden');
+    menuClose.classList.remove('hidden');
+    dropMenu.classList.remove('hidden');
+})
+menuClose.addEventListener('click', function(){
+    menuClose.classList.add('hidden');
+    menuOpen.classList.remove('hidden');
+    dropMenu.classList.add('hidden');
+})
