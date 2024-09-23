d49d25b7fb49bc95691de943544c278e5137597f
mdabdulkyum1 <kyummdabdul@gmail.com>
2024-09-23 10:46:24 +0600

    show and hide  data display done

--- js/main.js
+++ js/main.js
@@ -1,1 +1,27 @@
-console.log("Main.js")
+// select all elements
+const donationBtn = document.getElementById('donation-btn')
+const historyBtn = document.getElementById('history-btn')
+const historySection = document.getElementById('history-section');
+const donationCards = document.getElementById('donation-cards');
+
+
+/*
+* Toggle the active status of the "Donation" and "History" buttons: 
+* data and display
+*/ 
+donationBtn.addEventListener('click', function(){
+    historyBtn.classList.remove('bg-primary')
+    donationBtn.classList.add('bg-primary')
+    // switch donation Data
+    donationCards.classList.remove('hidden')
+    historySection.classList.add('hidden')
+})
+historyBtn.addEventListener('click', function(){
+    donationBtn.classList.remove('bg-primary')
+    historyBtn.classList.add('bg-primary')
+    // switch History Data
+    donationCards.classList.add('hidden')
+    historySection.classList.remove('hidden')
+})
+
+
--- js/utilities.js
+++ js/utilities.js
@@ -1,1 +1,18 @@
-console.log("utilities.js")
+// date & time 
+function nowTime(){
+    const now = new Date();
+    return now;
+}
+
+// get input Filed Value
+function getInputFiledValueById(id){
+    const filedValue = document.getElementById(id).value;
+    const filedNumber = parseFloat(filedValue);
+    return filedNumber;
+}
+// get elements text 
+function getElementTextById(id){
+    const elText = document.getElementById(id).innerText;
+    const elTextNumber = parseFloat(elText);
+    return elTextNumber;
+}
