c6834df8e9a6c379d4a2ad8d8e2f3fb3a72ebe7d
mdabdulkyum1 <kyummdabdul@gmail.com>
2024-09-23 17:29:44 +0600

    3 cards functionalities done

--- js/main.js
+++ js/main.js
@@ -4,8 +4,9 @@ const historyBtn = document.getElementById('history-btn')
 const historySection = document.getElementById('history-section');
 const donationCards = document.getElementById('donation-cards');
 const donateBtnN = document.getElementById('donate-btn-n');
+const donateBtnF = document.getElementById('donate-btn-f');
+const donateBtnQ = document.getElementById('donate-btn-q')
 const donateHistory = document.getElementById('donate-history');
-
 /*
 * Toggle the active status of the "Donation" and "History" buttons: 
 * data and display
@@ -54,6 +55,63 @@ donateBtnN.addEventListener('click', function(){
 
    
     getElById('donate-input-amount-n').value = "";
+});
+// function for card 2
+donateBtnF.addEventListener('click', function(){
+    // call functions and get value
+    const accountBalance = getElementTextById('account-balance'); // main balance
+    const donatedAmount =  getElementTextById('donated-amount-f');
+    const donateAmount =   getInputFiledValueById('donate-input-f');
+    const totalDonate = donatedAmount + donateAmount; 
+    const newBalance = accountBalance - donateAmount;
+    // Input Validation
+    if(isNaN(donateAmount) || donateAmount <= 0 || donateAmount > accountBalance){
+        alert(" invalid Donation amount")
+    }else{
+        getElById('donated-amount-f').innerText = totalDonate;
+        getElById('account-balance').innerText = newBalance;
+        getElById('modal').classList.remove('hidden');
+
+        // get data 
+        const infoF = getElById('info-f').innerText;
+        donateHistory.innerHTML += `
+        <div class="border border-gray-200 rounded-lg p-8">
+            <h3 class="text-2xl font-extrabold my-3">${donateAmount} Taka is ${infoF}</h3>
+                    <p>${nowTime()}</p>
+                </div>
+    `;
+    }
+
+   
+    getElById('donate-input-f').value = "";
+});
+// function for card 3
+donateBtnQ.addEventListener('click', function(){
+    // call functions and get value
+    const accountBalance = getElementTextById('account-balance'); // main balance
+    const donatedAmount =  getElementTextById('donate-amount-q');
+    const donateAmount =   getInputFiledValueById('donate-input-q');
+    const totalDonate = donatedAmount + donateAmount; 
+    const newBalance = accountBalance - donateAmount;
+    // Input Validation
+    if(isNaN(donateAmount) || donateAmount <= 0 || donateAmount > accountBalance){
+        alert(" invalid Donation amount")
+    }else{
+        getElById('donate-amount-q').innerText = totalDonate;
+        getElById('account-balance').innerText = newBalance;
+        getElById('modal').classList.remove('hidden');
+
+        // get data 
+        const infoQ = getElById('info-q').innerText;
+        donateHistory.innerHTML += `
+        <div class="border border-gray-200 rounded-lg p-8">
+            <h3 class="text-2xl font-extrabold my-3">${donateAmount} Taka is ${infoQ}</h3>
+                    <p>${nowTime()}</p>
+                </div>
+    `;
+    }
+
+    getElById('donate-input-q').value = "";
 });
 
 // close modal
